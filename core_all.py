0a44e0f4be5ca27e85026794ee39820d8936f761
timpelplomp <timpelplomp@googlemail.com>
2019-07-14 00:05:26 +0200

    working stanford implementation

--- core_all.py
+++ core_all.py
@@ -1,9 +1,11 @@
 # planning to tag all
-import os
 from stanfordcorenlp import StanfordCoreNLP
-
+from pathlib import Path
 # need to set the java environment first
 # os.environ["JAVAHOME"] = "E:/Coding/jdks/jdk-12.0.1/bin/java.exe"
+#  java -mx16g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 25000 -MaxCharLength
+#  100000000000
+# from commandline to have a server at localhost
 
 
 def core_all(input_dir, output_dir, memory="8g"):
@@ -12,16 +14,20 @@ def core_all(input_dir, output_dir, memory="8g"):
     # adjust parameters to your liking, it is called for each annotation
     properties = {'annotators': 'tokenize,ssplit,pos,lemma', 'pipelineLanguage': 'en', 'outputFormat': 'xml'}
     # the full server is started (negligible impact if inputs are large enough)
-    with StanfordCoreNLP(corenlp_path, memory=memory) as corenlp:
 
-        for input_file in os.listdir(input_dir):
-            basename = os.path.basename(input_file)
-            output_file_path = os.path.join(output_dir, basename)
+    # I hate having to do this, maybe we should switch to a different wrapper or roll our own
+    # with StanfordCoreNLP(corenlp_path, memory=memory) as corenlp:
+    with StanfordCoreNLP('http://localhost', port=9000) as corenlp:
+        for input_file in Path(input_dir).glob("*.txt"):
+            basename = input_file.stem
+            print("looking at: " + str(basename))
+            # should work on this
+            output_file_path = Path(output_dir).joinpath(basename + ".xml")
 
-            with open(input_file, "r", encoding="utf-8") as i:
+            with input_file.open(mode="r", encoding="utf-8") as i:
                 input_text = i.read()
 
             output_xml = corenlp.annotate(input_text, properties=properties)
 
-            with open(output_file_path, "w", encoding="utf-8") as f:
+            with output_file_path.open(mode="w", encoding="utf-8") as f:
                 f.write(output_xml)
---
+++ kick_off.py
@@ -0,0 +1,7 @@
+from core_all import core_all
+
+# example
+input_dir = "resources/txt_in/neg"
+output_dir = "resources/tagged_in/neg"
+# starts programme
+core_all(input_dir, output_dir)
