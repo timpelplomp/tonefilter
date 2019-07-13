0cb5e8cc42b60757ec41bd05eef6cbb957e9fc22
timpelplomp <timpelplomp@googlemail.com>
2019-07-13 22:27:44 +0200

    working stanford implementation

--- tag_all.py
+++ tag_all.py
@@ -1,1 +1,13 @@
 # planning to tag all
+import os
+from stanfordcorenlp import StanfordCoreNLP
+
+# need to set the java environment first
+# os.environ["JAVAHOME"] = "E:/Coding/jdks/jdk-12.0.1/bin/java.exe"
+
+sentence = "I dislike the oracle java standard."
+corenlp_path = "D:/to keep/Tools/Stanford/stanford-corenlp-full-2018-10-05"
+properties = {'annotators': 'tokenize,ssplit,pos,lemma', 'pipelineLanguage': 'en', 'outputFormat': 'xml'}
+with StanfordCoreNLP(corenlp_path, memory='8g',) as corenlp:
+
+    print(corenlp.annotate(sentence, properties=properties))
