660df5132060b8fe81fa62bbc5524932e5081e17
timpelplomp <timpelplomp@googlemail.com>
2019-07-14 16:05:57 +0200

    dictionary analysis started

---
+++ dictionary_creation.py
@@ -0,0 +1,29 @@
+import counter
+from readfile import *
+from pathlib import Path
+
+# # all
+# lemma_core_nlp_dict = counter.count_files_in_dir("F:/Studium/resources/out_corpusexample-ling1/corenlp",
+#                                                  x_path=".//lemma")
+#
+# lemma_count_dict = {}
+# for key in lemma_core_nlp_dict.keys():
+#     lemma_count_dict[key] = len(lemma_core_nlp_dict[key])
+# print(lemma_count_dict)
+
+
+def create_lemma_dict(xml_source_dir, output_path):
+    for xml_file in Path(xml_source_dir).glob("*.xml"):
+        string_path = str(xml_file.resolve())
+        print(string_path)
+        lemma_core_nlp_dict = counter.count_files_in_dir(xml_source_dir,
+                                                         x_path=".//lemma")
+        print(lemma_core_nlp_dict)
+        dump_into_json(Path(output_path), xml_file.stem + ".json", lemma_core_nlp_dict)
+        dump_into_csv(output_path, xml_file.stem + ".csv", lemma_core_nlp_dict)
+
+
+# quick example
+in_neg_dir = "resources/tagged_in/neg"
+out_neg_dir = "resources/dicts/neg"
+create_lemma_dict(in_neg_dir, out_neg_dir)
