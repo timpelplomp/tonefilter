f08bd17d2400d46b49ec6d1dae4c4d0373517790
timpelplomp <timpelplomp@googlemail.com>
2019-09-14 16:02:41 +0200

    added validation

--- analyse.py
+++ analyse.py
@@ -15,8 +15,12 @@ def analyse_sent(sentence, pos_fp="final_dict/pos_dict_cut.json", neg_fp="final_
             final_score += 1
         elif lemma in neg_dict:
             final_score -= 1
-
-    return final_score
+    if final_score > 1:
+        return 1
+    # elif final_score == 0:
+    #     return 0
+    else:
+        return 0
 
 
 # https://www.machinelearningplus.com/nlp/lemmatization-examples-python/
@@ -30,5 +34,3 @@ def lemmatise(sentence):
     lemmatized_list = [wd.lemmatize(tag) for wd, tag in words_and_tags]
     return lemmatized_list
 
-
-print(analyse_sent("Joy is its own reward."))
---
+++ validate.py
@@ -0,0 +1,26 @@
+import analyse
+
+
+def test(sent_path):
+    correct = 0
+    with open(sent_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    total = len(lines)
+    for line in lines:
+        sentence = line.split("#")[0]
+        validation = line.split("#")[1]
+        analysis = analyse.analyse_sent(sentence)
+        if int(validation) == analysis:
+            correct += 1
+
+        print("sentence: " + sentence)
+        print("validation: " + validation)
+        print("analysis: " + str(analysis))
+
+        print("BREAK")
+
+    acc_percentage = round(correct/total * 100, 2)
+    print("Final ACC: " + str(acc_percentage))
+
+
+test("resources/testdata/sentiment_saetze.txt")
