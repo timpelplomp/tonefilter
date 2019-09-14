8bd495c049600321be35aa8d83ebc944b4c5daa1
timpelplomp <timpelplomp@googlemail.com>
2019-09-14 11:44:21 +0200

    dictionary analysis started

---
+++ dict_comparison.py
@@ -0,0 +1,30 @@
+import readfile
+import json
+
+
+def create_both(raw_neg_dict, raw_pos_dict, path="final_dict"):
+    raw_neg_dict_size = sum(raw_neg_dict.values())
+    raw_pos_dict_size = sum(raw_pos_dict.values())
+
+    freq_pos_dict = {key: (value/raw_pos_dict_size)*100 for (key, value) in raw_pos_dict}
+    freq_neg_dict = {key: (value / raw_neg_dict_size) * 100 for (key, value) in raw_neg_dict}
+
+    cut_off_neg = cut_off_dict(freq_neg_dict, freq_pos_dict)
+    cut_off_pos = cut_off_dict(freq_pos_dict, freq_neg_dict)
+
+    readfile.dump_into_json(path, "pos_dict_cut.json", cut_off_pos)
+    readfile.dump_into_json(path, "neg_dict_cut.json", cut_off_neg)
+
+
+def cut_off_dict(input_dict, compare_dict, tolerance=10):
+    new_dict = {key: value for (key, value) in input_dict
+                if value not in compare_dict or value > compare_dict[key] + tolerance}
+
+    return new_dict
+
+
+def wrap():
+    pos_dict = json.load("resources/dicts/neg/draculafirst.json")
+    neg_dict = json.load("dasdasdsa")
+
+    create_both(neg_dict, pos_dict)
