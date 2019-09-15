c10ffbcb5d4520eef5507070a53a284a00d3add2
timpelplomp <timpelplomp@googlemail.com>
2019-09-15 14:58:32 +0200

    added quick Bayes

--- texblob_ex.py
+++ texblob_ex.py
@@ -1,4 +1,5 @@
 from textblob import TextBlob
+from textblob.en.sentiments import NaiveBayesAnalyzer
 
 
 def test_blob(sentence):
@@ -9,5 +10,15 @@ def test_blob(sentence):
         return 0
     elif polarity > 0:
         return 1
+    else:
+        return -1
+
+
+def test_blob_bayes(sentence):
+    classification = NaiveBayesAnalyzer.analyze(NaiveBayesAnalyzer(), sentence).classification
+    if classification == "pos":
+        return 1
+    elif classification == "neg":
+        return 0
     else:
         return -1
--- validate.py
+++ validate.py
@@ -52,6 +52,31 @@ def test_blob(sent_path):
     print("num_pos: " + str(num_pos) + " out of " + str(total))
 
 
+def test_blob_bayes(sent_path):
+    num_pos = 0
+    correct = 0
+    with open(sent_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    total = len(lines)
+    for line in lines:
+        sentence = line.split("#")[0]
+        validation = line.split("#")[1]
+        analysis = texblob_ex.test_blob_bayes(sentence)
+        if int(validation) == analysis:
+            correct += 1
+        if int(validation) == 1:
+            num_pos += 1
+        print("sentence: " + sentence)
+        print("validation: " + validation)
+        print("analysis: " + str(analysis))
+
+        print("BREAK")
+
+    acc_percentage = round(correct/total * 100, 2)
+    print("Final ACC: " + str(acc_percentage))
+    print("num_pos: " + str(num_pos) + " out of " + str(total))
+
+
 path_sent = "resources/testdata/sentiment_saetze.txt"
 # test_own("resources/testdata/sentiment_saetze.txt")
-test_blob(path_sent)
+test_blob_bayes(path_sent)
