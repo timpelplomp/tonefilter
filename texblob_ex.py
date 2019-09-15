dd22475ad5f5641abcf81f7668e3b90e032a0c40
timpelplomp <timpelplomp@googlemail.com>
2019-09-15 16:04:26 +0200

    quick own bayes naive

--- texblob_ex.py
+++ texblob_ex.py
@@ -1,5 +1,6 @@
 from textblob import TextBlob
 from textblob.en.sentiments import NaiveBayesAnalyzer
+from textblob.classifiers import NaiveBayesClassifier
 
 
 def test_blob(sentence):
@@ -22,3 +23,22 @@ def test_blob_bayes(sentence):
         return 0
     else:
         return -1
+
+
+def test_naives_bayes_classifier_own(sentence):
+    train_tuples = read_sent_to_list()
+    classifier = NaiveBayesClassifier(train_tuples)
+    return classifier.classify(sentence)
+
+
+def read_sent_to_list(limit=50):
+    tuple_list = []
+
+    with open("resources/testdata/sentiment_saetze.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    for line in lines:
+        sentence = line.split("#")[0]
+        classification = int(line.split("#")[1])
+        tuple_list.append([sentence, classification])
+
+    return tuple_list[:limit]
--- validate.py
+++ validate.py
@@ -77,6 +77,31 @@ def test_blob_bayes(sent_path):
     print("num_pos: " + str(num_pos) + " out of " + str(total))
 
 
+def test_blob_naive_bayes_classifier_own(sent_path):
+    num_pos = 0
+    correct = 0
+    with open(sent_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()[50:]
+    total = len(lines)
+    for line in lines:
+        sentence = line.split("#")[0]
+        validation = line.split("#")[1]
+        analysis = texblob_ex.test_naives_bayes_classifier_own(sentence)
+        if int(validation) == analysis:
+            correct += 1
+        if int(validation) == 1:
+            num_pos += 1
+        print("sentence: " + sentence)
+        print("validation: " + validation)
+        print("analysis: " + str(analysis))
+
+        print("BREAK")
+
+    acc_percentage = round(correct/total * 100, 2)
+    print("Final ACC: " + str(acc_percentage))
+    print("num_pos: " + str(num_pos) + " out of " + str(total))
+
+
 path_sent = "resources/testdata/sentiment_saetze.txt"
 # test_own("resources/testdata/sentiment_saetze.txt")
-test_blob_bayes(path_sent)
+test_blob_naive_bayes_classifier_own(path_sent)
