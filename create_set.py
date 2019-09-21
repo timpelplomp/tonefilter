3ff0b3d6a0fceb0f0566506a291bbf5aa62d8a89
timpelplomp <timpelplomp@googlemail.com>
2019-09-22 01:14:56 +0200

    mood test sents

--- create_set.py
+++ create_set.py
@@ -49,7 +49,7 @@ def encode_text(blob, flag, until):
     return encoded_sents
 
 
-def shuffle_save_all(encoded_lists):
+def shuffle_save_all(encoded_lists, path_ef="total_train_sets"):
     random.shuffle(encoded_lists)
 
     enc_sents = []
@@ -58,8 +58,8 @@ def shuffle_save_all(encoded_lists):
         enc_sents.append(sublist[0])
         cl_sents.append(sublist[1])
 
-    readfile.dump_into_json("total_train_sets", "enc_sents.json", enc_sents)
-    readfile.dump_into_json("total_train_sets", "cl_sents.json", cl_sents)
+    readfile.dump_into_json(path_ef, "enc_sents.json", enc_sents)
+    readfile.dump_into_json(path_ef, "cl_sents.json", cl_sents)
 
 
 def get_both_corp():
@@ -72,6 +72,31 @@ def get_both_corp():
     shuffle_save_all(combined_list)
 
 
+def create_test_set():
+    with open("resources/testdata/sentiment_saetze.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    with open("id_dict/id_dict.json", "r", encoding="utf-8") as f:
+        encode_dict = json.load(f)
+
+    enc_list = []
+
+    for line in lines:
+        split_line = line.split("#")
+        sent_blob = TextBlob(split_line[0])
+        flag = int(split_line[1])
+
+        sublist = []
+        for word in sent_blob.words:
+            if word in encode_dict:
+                sublist.append(encode_dict[word])
+            else:
+                sublist.append(0)
+        enc_list.append([sublist, flag])
+
+    shuffle_save_all(enc_list, "resources/testdata")
+
 # readfile.dump_into_json("id_dict", "id_dict.json", freq_all())
 # encode_text(blob_it("drac"), 1)
 # get_both_corp()
+
+create_test_set()
